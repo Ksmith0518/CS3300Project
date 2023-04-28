6379f8a9e41a704287aea591c88cbe548416e61d
Ksmith0518 <ksmith0518@uccs.edu>
2023-04-28 04:19:11 +0000

    conflict resolutions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   resources :games
-<<<<<<< HEAD
-  root "projects#index"
-  resources :projects
-=======
   root "games#index"
   resources :games
->>>>>>> iteration01Dev
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
