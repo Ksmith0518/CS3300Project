8c3a1dad4c47b8c74fbacce24e7cb8bbc26ad38e
Ksmith0518 <ksmith0518@uccs.edu>
2023-04-28 04:33:37 +0000

    fixing migration issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_07_192147) do
+ActiveRecord::Schema.define(version: 2023_04_07_185808) do
 
   create_table "games", force: :cascade do |t|
     t.string "Title"
